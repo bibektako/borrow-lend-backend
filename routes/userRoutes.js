00090baac05ed155527ba31ee2b08ea70b18c2d4
bibektako <133769273+bibektako@users.noreply.github.com>
2025-06-13 16:49:51 +0545

    added path of signup and signin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/userControllers");
 const { userValidationRules, validate} = require("../middlewares/userValidation")
